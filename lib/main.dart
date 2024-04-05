799097b6e1c8787981771f431ce4c81cf14b75c1
RuslanAkhmetov <akhmrusl@gmail.com>
2024-04-05 16:24:57 +0300

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
-const int maxSeed = 250;
+const int maxSeeds = 250;
 
-void main() => runApp(const Sunflower extends StatefulWidget());
+void main() => runApp(const Sunflower());
 
 class Sunflower extends StatefulWidget {
-  const Sunflower ({super.key});
+  const Sunflower({super.key});
 
   @override
   State<StatefulWidget> createState() {
@@ -16,70 +15,91 @@ class Sunflower extends StatefulWidget {
 }
 
 class _SunflowerState extends State<Sunflower> {
-  int seeds = maxSeed ~/2;
+  int seeds = maxSeeds ~/ 2;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        brightness: Brightness.light,
-        appBarTheme: const AppBarTheme(elevation: 2),
-      ),
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Sunflower')
+        theme: ThemeData(
+          brightness: Brightness.light,
+          appBarTheme: const AppBarTheme(elevation: 2),
         ),
-        body: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
-                child: SunflowerWidget (seeds),
-              ),
-              const SizedBox(height: 20),
-              Text('Showing ${seeds.round()} seeds'),
-              SizedBox(
-                width: 300,
-                child: Slider(
-                  min: 1,
-                  max: maxSeed.toDouble(),
-                  value: seeds.toDouble(),
-                  onChanged: (val) {
-                    setState(()=>seeds=val.round());
-                  },
-                ),
-              ),
-              const SizedBox(height: 20,)
-            ],
-          )
-        ),
-      )
-    )
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+              title: const Text('Sunflower')
+          ),
+          body: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: SunflowerWidget(seeds),
+                  ),
+                  const SizedBox(height: 20),
+                  Text('Showing ${seeds.round()} seeds'),
+                  SizedBox(
+                    width: 300,
+                    child: Slider(
+                      min: 1,
+                      max: maxSeeds.toDouble(),
+                      value: seeds.toDouble(),
+                      onChanged: (val) {
+                        setState(() => seeds = val.round());
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 20,)
+                ],
+              )
+          ),
+        )
+    );
   }
 }
 
 class SunflowerWidget extends StatelessWidget {
-  static const tau = math.pi *2;
-  static const scaleFactor = 1/40;
+  static const tau = math.pi * 2;
+  static const scaleFactor = 1 / 40;
   static const size = 600.0;
-  static final phi = (math.sqrt(5)+1)/2;
+  static final phi = (math.sqrt(5) + 1) / 2;
   static final rng = math.Random();
   final int seeds;
+
   const SunflowerWidget(this.seeds, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    final seedWidgets = <Widget> [];
-    for(var i = 0; i < seeds; i++){
-      final theta = i * tau/phi;
-      final r = math.sqrt(i)*scaleFactor;
+    final seedWidgets = <Widget>[];
+    for (var i = 0; i < seeds; i++) {
+      final theta = i * tau / phi;
+      final r = math.sqrt(i) * scaleFactor;
       seedWidgets.add(AnimatedAlign(
-          key: ValueKey(i),
-          duration: Duration(microseconds: rng.nextInt(500) + 250),
-          curve: Curves.easeInOut,
-          alignment: Alignment(r+math.cos(theta), -1 * r * math.sin(theta)),
-          child: const Dot (true),));
+        key: ValueKey(i),
+        duration: Duration(microseconds: rng.nextInt(500) + 250),
+        curve: Curves.easeInOut,
+        alignment: Alignment(r + math.cos(theta), -1 * r * math.sin(theta)),
+        child: const Dot (true),));
     }
+    for (var j = seeds; j < maxSeeds; j++) {
+      final x = math.cos(tau + j / (maxSeeds - 1)) * 0.9;
+      final y = math.sin(tau + j / (maxSeeds - 1)) * 0.9;
+      seedWidgets.add(AnimatedAlign(
+        key: ValueKey(j),
+        curve: Curves.easeInOut,
+        alignment: Alignment(x, y),
+        duration: Duration(microseconds: rng.nextInt(500) + 250),
+        child: const Dot(false),
+      ));
+    }
+    return FittedBox(
+      fit: BoxFit.contain,
+      child: SizedBox(
+        height: size,
+        width: size,
+        child: Stack(children: seedWidgets,),
+      ),
+    );
   }
 }
 
@@ -87,10 +107,18 @@ class Dot extends StatelessWidget {
   static const size = 5.0;
   static const radius = 5.0;
   final bool lit;
-  const Dot (this.lit, {super.key});
+
+  const Dot(this.lit, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return DecoratedBox(decoration: decoration)
+    return DecoratedBox(decoration: BoxDecoration(
+        color: lit ? Colors.orange : Colors.grey.shade700,
+        borderRadius: BorderRadius.circular(radius)
+    ),
+      child: const SizedBox(
+        height: size,
+        width: size,
+      ),);
   }
 }
