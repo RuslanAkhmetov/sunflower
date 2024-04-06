b125e3e74ff7297bc4147c2089ade38d1a111a55
RuslanAkhmetov <akhmrusl@gmail.com>
2024-04-06 20:12:51 +0300

    Add second slider

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class Sunflower extends StatefulWidget {
 
 class _SunflowerState extends State<Sunflower> {
   int seeds = maxSeeds ~/ 2;
+  int curveParameter = 5;
 
   @override
   Widget build(BuildContext context) {
@@ -26,35 +27,48 @@ class _SunflowerState extends State<Sunflower> {
         ),
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-          appBar: AppBar(
-              title: const Text('Sunflower')
-          ),
+          appBar: AppBar(title: const Text('Sunflower')),
           body: Center(
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: SunflowerWidget(seeds),
-                  ),
-                  const SizedBox(height: 20),
-                  Text('Showing ${seeds.round()} seeds'),
-                  SizedBox(
-                    width: 300,
-                    child: Slider(
-                      min: 1,
-                      max: maxSeeds.toDouble(),
-                      value: seeds.toDouble(),
-                      onChanged: (val) {
-                        setState(() => seeds = val.round());
-                      },
-                    ),
-                  ),
-                  const SizedBox(height: 20,)
-                ],
-              )
-          ),
-        )
-    );
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: SunflowerWidget(seeds, curveParameter),
+              ),
+              const SizedBox(height: 20),
+              Text('Showing ${seeds.round()} seeds'),
+              SizedBox(
+                width: 300,
+                child: Slider(
+                  min: 1,
+                  max: maxSeeds.toDouble(),
+                  value: seeds.toDouble(),
+                  onChanged: (val) {
+                    setState(() => seeds = val.round());
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text('Change curve parameter $curveParameter'),
+              SizedBox(
+                width: 300,
+                child: Slider(
+                  min: 1,
+                  max: 100,
+                  value: curveParameter.toDouble(),
+                  onChanged: (val) {
+                    setState(() => curveParameter = val.round());
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+            ],
+          )),
+        ));
   }
 }
 
@@ -62,11 +76,12 @@ class SunflowerWidget extends StatelessWidget {
   static const tau = math.pi * 2;
   static const scaleFactor = 1 / 40;
   static const size = 600.0;
-  static final phi = (math.sqrt(5) + 1) / 2;
+  final int curveParameter;
+  static var phi = (math.sqrt(5) + 1) / 2;
   static final rng = math.Random();
   final int seeds;
 
-  const SunflowerWidget(this.seeds, {super.key});
+  const SunflowerWidget(this.seeds, this.curveParameter, {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -74,13 +89,14 @@ class SunflowerWidget extends StatelessWidget {
     for (var i = 0; i < seeds; i++) {
       final theta = i * tau / phi;
       final r = math.sqrt(i) * scaleFactor;
-
+      phi = (math.sqrt(curveParameter) + 1) / 2;
       seedWidgets.add(AnimatedAlign(
         key: ValueKey(i),
-        duration: Duration(microseconds: rng.nextInt(500) + 250),
+        duration: Duration(milliseconds: rng.nextInt(500) + 250),
         curve: Curves.easeInOut,
         alignment: Alignment(r * math.cos(theta), -1 * r * math.sin(theta)),
-        child: const Dot (true),));
+        child: const Dot(true),
+      ));
     }
     for (var j = seeds; j < maxSeeds; j++) {
       final x = math.cos(tau * j / (maxSeeds - 1)) * 0.9;
@@ -89,7 +105,7 @@ class SunflowerWidget extends StatelessWidget {
         key: ValueKey(j),
         curve: Curves.easeInOut,
         alignment: Alignment(x, y),
-        duration: Duration(microseconds: rng.nextInt(500) + 250),
+        duration: Duration(milliseconds: rng.nextInt(500) + 250),
         child: const Dot(false),
       ));
     }
@@ -98,7 +114,9 @@ class SunflowerWidget extends StatelessWidget {
       child: SizedBox(
         height: size,
         width: size,
-        child: Stack(children: seedWidgets,),
+        child: Stack(
+          children: seedWidgets,
+        ),
       ),
     );
   }
@@ -113,13 +131,14 @@ class Dot extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DecoratedBox(decoration: BoxDecoration(
-        color: lit ? Colors.orange : Colors.grey.shade700,
-        borderRadius: BorderRadius.circular(radius)
-    ),
+    return DecoratedBox(
+      decoration: BoxDecoration(
+          color: lit ? Colors.orange : Colors.grey.shade700,
+          borderRadius: BorderRadius.circular(radius)),
       child: const SizedBox(
         height: size,
         width: size,
-      ),);
+      ),
+    );
   }
 }
